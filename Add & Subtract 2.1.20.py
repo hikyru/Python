09df3a72e4bd168aa9cd1c98fe932ca7e918d428
hikyru <47800152+hikyru@users.noreply.github.com>
2020-03-21 18:24:14 -0700

    Add & Subtract numerals

---
+++ Add & Subtract 2.1.20.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 1 7:41 2020
+
+@author: KatherineYu
+"""
+
+'''
+a = 1   # variable
+b = 2   # variable
+print(a + b)   # addition
+print(b - a) # subtraction
+print(a * b) # multiplication
+print(b/a) # division
+'''
+
+a = int(input("Enter first number: "))    # variable
+b = int(input("Enter second number: "))   # variable
+
+
+print(a + b)   # addition
+print(b - a) # subtraction
+print(a * b) # multiplication
+print(b/a) # division
